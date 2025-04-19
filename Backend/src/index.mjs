ac07349c92faea3f28b1161d53bcec6d4b13f368
Fortune Obioha <fortuneobioha926@gmail.com>
2025-04-19 10:02:05 +0100

    added domain name to backend

--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://music-world-virid.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
